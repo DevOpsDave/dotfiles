89c8cd10b9669500261e46356161d5b2f22592ef
Dave <dave.barcelo@bazaarvoice.com>
2012-06-01 11:12:08 -0500

    added stuffs

--- bashrc.d/.bashrc.d/bazaarvoice.sh
+++ bashrc.d/.bashrc.d/bazaarvoice.sh
@@ -1,1 +1,2 @@
-alias=get_hosts='curl -s https://awsmanager.lab.bazaarvoice.com/rundeckinventory'
+alias get_hosts='curl -s https://awsmanager.lab.bazaarvoice.com/rundeckinventory'
+alias ssh-rs='ssh -t -D 8080 -p 24701 bvrack-bastion@rs-bastion.bazaarvoice.com '\''ssh dbarcelo@10.10.35.26'\'''
